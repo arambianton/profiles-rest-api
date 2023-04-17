51b1f9cc9881f7d80bf4bc10ef77f997b1b01f27
Anton <arambianton@gmail.com>
2023-04-17 11:40:32 +0300

    Changed Allowed hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -27,7 +27,8 @@ DEBUG = bool(int(os.environ.get('DEBUG', 1)))
 
 ALLOWED_HOSTS = [
     'https://ec2-16-16-70-141.eu-north-1.compute.amazonaws.com/', 
-    '127.0.0.1'
+    '127.0.0.1',
+    '95.165.160.192'
 ]
 
 
